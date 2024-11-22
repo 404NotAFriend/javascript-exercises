79e35d20aff333381db5d82903e0642d11e1fc22
SkilleR <169854673+404NotAFriend@users.noreply.github.com>
2024-11-22 23:46:10 +0100

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+//Attributed string name to function
+const reverseString = function (string) {
+  //Created variable to split the text in separate letters
+  let stringSplit = string.split("");
+  //Created variable to call for the reverse on the separate letters
+  let reverseArray = stringSplit.reverse();
+  //Created variable to join the separate letters back together after being already reversed
+  let joinArray = reverseArray.join("");
+  // asked for the full word already reversed
+  return joinArray;
 };
+
+// Resolution after searching google and reading and learning at:
+//https://www.naukri.com/code360/library/how-to-reverse-a-string-in-javascript
 
 // Do not edit below this line
 module.exports = reverseString;
