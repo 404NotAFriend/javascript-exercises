ec19084daf51269e5ebb176c6e8a85f2775084c3
SkilleR <169854673+404NotAFriend@users.noreply.github.com>
2024-12-03 19:00:37 +0100

    Create base function and declared 2 variables || Create a check to see if values are positive and integers || Used typeof to check for numbers and value  < 0 for positive || Created variable for sum of all valors || Create a loop that goes throught the Minimum value given to first or last || Used Math.min and max to order the values from lower to upper Created a line to sum all the values between first and last , including first and last ||

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function () {};
+// create base function and declared 2 variables
+
+const sumAll = function (first, last) {
+  // Create a check to see if values are positive and integers
+  // Used typeof to check for numbers and value  < 0 for positive
+
+  if (
+    typeof first !== "number" ||
+    typeof last !== "number" ||
+    first < 0 ||
+    last < 0
+  ) {
+    return "ERROR";
+  }
+
+  //Created variable for sum of all valors
+
+  let sum = 0;
+
+  //Create a loop that goes throught the Minimum value given to first or last
+  // USed Math.min and max to order the values from lower to upper
+  // Created a line to sum all the values between first and last , including first and last
+  for (let i = Math.min(first, last); i <= Math.max(first, last); i++) {
+    sum += i;
+  }
+  return sum;
+};
 
 // Do not edit below this line
 module.exports = sumAll;
