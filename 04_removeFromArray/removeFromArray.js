72cee6169ddb81b3bb823854a69dfc610fac9312
SkilleR <169854673+404NotAFriend@users.noreply.github.com>
2024-12-03 17:28:07 +0100

    Resolved Exercises and ran all  tests sucessfully.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array2, ...args) {
+  const newArray = [];
+  array2.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+1;
