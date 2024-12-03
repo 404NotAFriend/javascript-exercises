2233b156015c2789a76d44746e5587f6e3e235f0
SkilleR <169854673+404NotAFriend@users.noreply.github.com>
2024-12-03 21:49:06 +0100

    Created basic code on exercise 07 , created converToCelsius and Fahrenheit functions Both functions passed the first code of making the correct conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return (celsius * 5) / 9 + 32;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
