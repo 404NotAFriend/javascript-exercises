afb1f51700e0296d611cfd5e790498e5fd28e769
SkilleR <169854673+404NotAFriend@users.noreply.github.com>
2025-01-03 20:14:08 +0100

    Created power function Created Factiorialize function with a for loop as Iterative Approach to avoid overhead recusive function calls Completed all tests sucessfully Exercise completed - 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -5,11 +5,19 @@ const subtract = (a, b) => a - b;
 
 const sum = (arr) => arr.reduce((initial, current) => initial + current, 0);
 
-const multiply = (arr) => arr.reduce((initial, current) => initial * current, 0);
+const multiply = (arr) => arr.reduce((initial, current) => initial * current, 1);
 
-const power = function () {};
+const power = (base, exponent) => base ** exponent;
 
-const factorial = function () {};
+const factorial = (f) => {
+  if (f < 0) throw new Error("Factorial is not defined for negative numbers."); // created error for negative numbers
+  let result = 1;
+  for (let i = 2; i <= f; i++) {
+    //used i = 2  to avoid multiplying by 1 on the first iteration, which is redundant.
+    result *= i;
+  }
+  return result;
+};
 
 // Do not edit below this line
 module.exports = {
