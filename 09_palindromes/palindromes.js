af289cb9936eb939743e573d225b6be304220bfb
SkilleR <169854673+404NotAFriend@users.noreply.github.com>
2025-01-04 01:17:21 +0100

    Started and completed successfully all tests for project 09 palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
-};
+const palindromes = (test) =>
+  test
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("")
+    .reverse()
+    .join("") === test.toLowerCase().replace(/[^a-z0-9]/g, "");
 
 // Do not edit below this line
 module.exports = palindromes;
