f409393e0894e2a1a73c01dc5ed524cd5a17f09c
SkilleR <169854673+404NotAFriend@users.noreply.github.com>
2025-01-05 01:47:27 +0100

    Created arrow function for calculating whos the oldest of array data Created 2 ifs , one in case no date of death is provided , sets it to present date In case the oldest is the one without Death Data , if makes first data on array start as older then runs function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = (people) =>
+  people.reduce((oldest, person) => {
+    if (person.yearOfDeath === undefined) {
+      const currentYear = new Date().getFullYear();
+      person.yearOfDeath = currentYear;
+    }
 
-};
+    // On first iteration, set the first person as the oldest
+    if (!oldest.yearOfDeath) {
+      return person;
+    }
+
+    const age = person.yearOfDeath - person.yearOfBirth;
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+
+    return age > oldestAge ? person : oldest;
+  }, {});
 
 // Do not edit below this line
 module.exports = findTheOldest;
